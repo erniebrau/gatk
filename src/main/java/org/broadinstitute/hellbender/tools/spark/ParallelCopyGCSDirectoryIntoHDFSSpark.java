e57c8d31cef8bbf0eefc2a3b4a2dd53cd932dad9
Chris Whelan <cwhelan@broadinstitute.org>
2017-03-29 16:09:33 -0400

    remove dependency on dataflow sdk

--- src/main/java/org/broadinstitute/hellbender/tools/spark/ParallelCopyGCSDirectoryIntoHDFSSpark.java
+++ src/main/java/org/broadinstitute/hellbender/tools/spark/ParallelCopyGCSDirectoryIntoHDFSSpark.java
@@ -1,9 +1,7 @@
 package org.broadinstitute.hellbender.tools.spark;
 
-import com.google.cloud.dataflow.sdk.util.GcsUtil;
-import com.google.cloud.dataflow.sdk.util.gcsfs.GcsPath;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.*;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.broadinstitute.barclay.argparser.Argument;
@@ -13,12 +11,16 @@ import org.broadinstitute.hellbender.engine.AuthHolder;
 import org.broadinstitute.hellbender.engine.spark.GATKSparkTool;
 import org.broadinstitute.hellbender.exceptions.GATKException;
 import org.broadinstitute.hellbender.utils.gcs.BucketUtils;
+import org.broadinstitute.hellbender.utils.io.IOUtils;
 import scala.Tuple2;
 
 import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.SeekableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * This tool uses Spark to do a parallel copy of either a file or a directory from GCS into HDFS.
@@ -51,36 +53,37 @@ public class ParallelCopyGCSDirectoryIntoHDFSSpark extends GATKSparkTool {
         final String inputGCSPathFinal = inputGCSPath;
         final String outputDirectoryFinal = outputHDFSDirectory;
 
-        Path file = new Path(outputHDFSDirectory);
+        org.apache.hadoop.fs.Path file = new org.apache.hadoop.fs.Path(outputHDFSDirectory);
 
         try(FileSystem fs = file.getFileSystem(new Configuration())){
 
             fs.mkdirs(file);
             final long chunkSize = Long.parseLong(fs.getConf().get("dfs.blocksize"));
 
-            final GcsUtil gcsUtil = new GcsUtil.GcsUtilFactory().create(auth.asPipelineOptionsDeprecated());
-
-            final List<GcsPath> gcsPaths;
-            if (inputGCSPath.endsWith("/")) {
-                gcsPaths = gcsUtil.expand(GcsPath.fromUri(inputGCSPathFinal + "*"));
+            final List<Path> gcsPaths;
+            final Path inputGCSNIOPath = IOUtils.getPath(inputGCSPathFinal);
+            if (Files.isDirectory(inputGCSNIOPath)) {
+                logger.warn("directory: " + inputGCSPathFinal);
+                gcsPaths = Files.list(inputGCSNIOPath).collect(Collectors.toList());
             } else {
-                gcsPaths = Collections.singletonList(GcsPath.fromUri(inputGCSPathFinal));
+                logger.warn("single file");
+                gcsPaths = Collections.singletonList(inputGCSNIOPath);
             }
 
             List<Tuple2<String, Integer>> chunkList = new ArrayList<>();
-            for (GcsPath path : gcsPaths) {
+            for (Path path : gcsPaths) {
 
-                final String filePath = path.toString();
-                if (path.endsWith("/")) {
+                if (Files.isDirectory(path)) {
                     logger.info("skipping directory " + path);
                     continue;
                 }
-                final long size = BucketUtils.fileSize(filePath, auth.asPipelineOptionsDeprecated());
+
+                final long size = Files.size(path);
                 final long chunks = size / chunkSize + 1;
                 logger.info("processing path " + path + ", size = " + size + ", chunks = " + chunks);
 
                 for (int i = 0; i < chunks; i++) {
-                    chunkList.add(new Tuple2<>(filePath, i));
+                    chunkList.add(new Tuple2<>(path.toUri().toString(), i));
                 }
             }
 
@@ -91,21 +94,21 @@ public class ParallelCopyGCSDirectoryIntoHDFSSpark extends GATKSparkTool {
 
             final JavaPairRDD<String, Integer> chunkRDD = ctx.parallelizePairs(chunkList, chunkList.size());
 
-            final String fsURI = fs.getUri().toString();
-            logger.warn("FS URI: "+ fsURI);
-
             final JavaPairRDD<String, Tuple2<Integer, String>> chunkMappingRDD =
                     chunkRDD.mapToPair(p -> new Tuple2<>(p._1(), readChunkToHdfs(auth, p._1(), chunkSize, p._2(), outputDirectoryFinal)));
 
             final Map<String, Iterable<Tuple2<Integer, String>>> chunksByFilePath = chunkMappingRDD.groupByKey().collectAsMap();
 
-            for (GcsPath path : gcsPaths) {
+            for (Path path : gcsPaths) {
 
-                final String filePath = path.toString();
+                if (Files.isDirectory(path)) {
+                    continue;
+                }
 
+                final String filePath = path.toUri().toString();
                 final Iterable<Tuple2<Integer, String>> chunkListForFile = chunksByFilePath.get(filePath);
                 final String basename = path.getName(path.getNameCount() - 1).toString();
-                final Path outFilePath = new Path(outputDirectoryFinal + "/" + basename);
+                final org.apache.hadoop.fs.Path outFilePath = new org.apache.hadoop.fs.Path(outputDirectoryFinal + "/" + basename);
                 fs.createNewFile(outFilePath);
 
                 SortedMap<Integer, String> chunkMap = new TreeMap<>();
@@ -113,13 +116,13 @@ public class ParallelCopyGCSDirectoryIntoHDFSSpark extends GATKSparkTool {
                     chunkMap.put(entry._1(), entry._2());
                 }
 
-                Path[] chunkPaths = new Path[chunkMap.size()];
+                org.apache.hadoop.fs.Path[] chunkPaths = new org.apache.hadoop.fs.Path[chunkMap.size()];
 
                 final Iterator<Integer> iterator = chunkMap.keySet().iterator();
                 while (iterator.hasNext()) {
                     final Integer next =  iterator.next();
                     final String chunkPath = chunkMap.get(next);
-                    chunkPaths[next] = new Path(chunkPath);
+                    chunkPaths[next] = new org.apache.hadoop.fs.Path(chunkPath);
                 }
 
                 fs.concat(outFilePath, chunkPaths);
@@ -134,13 +137,13 @@ public class ParallelCopyGCSDirectoryIntoHDFSSpark extends GATKSparkTool {
     }
 
     private static final Tuple2<Integer, String> readChunkToHdfs(final AuthHolder authHolder, final String inputGCSPathFinal, final long chunkSize, final Integer chunkNum, final String outputDirectory) {
-        final GcsPath gcsPath = GcsPath.fromUri(inputGCSPathFinal);
+        final Path gcsPath = IOUtils.getPath(inputGCSPathFinal);
         final String basename = gcsPath.getName(gcsPath.getNameCount() - 1).toString();
-        Path outputPath = new Path(outputDirectory);
-        final String path = outputPath + "/" + basename + ".chunk." + chunkNum;
+        org.apache.hadoop.fs.Path outputPath = new org.apache.hadoop.fs.Path(outputDirectory);
+        final String chunkPath = outputPath + "/" + basename + ".chunk." + chunkNum;
 
-        try (SeekableByteChannel channel = new GcsUtil.GcsUtilFactory().create(authHolder.asPipelineOptionsDeprecated()).open(gcsPath);
-             final OutputStream outputStream = new BufferedOutputStream(BucketUtils.createFile(path, authHolder))){
+        try (SeekableByteChannel channel = Files.newByteChannel(gcsPath);
+             final OutputStream outputStream = new BufferedOutputStream(BucketUtils.createFile(chunkPath, authHolder))){
 
             final long start = chunkSize * (long) chunkNum;
             channel.position(start);
@@ -160,9 +163,9 @@ public class ParallelCopyGCSDirectoryIntoHDFSSpark extends GATKSparkTool {
                 byteBuffer.clear();
             }
         } catch (IOException e) {
-            throw new GATKException(e.getMessage(), e);
+            throw new GATKException(e.getMessage() + "; inputGCSPathFinal = " + inputGCSPathFinal, e);
         }
-        return new Tuple2<>(chunkNum, path);
+        return new Tuple2<>(chunkNum, chunkPath);
     }
 
 }
